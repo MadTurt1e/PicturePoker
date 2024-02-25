1f31feb064db3b1077b567f13a1407b0b0cc25de
MadTurt1e <81546749+MadTurt1e@users.noreply.github.com>
2024-02-24 22:59:11 -0500

    Pom file updated to run Jar file things again, + got rid of extraneous main class.

--- src/main/java/game/jdbc/picturepoker/JDBCExecutor.java
+++
@@ -1,7 +0,0 @@
-package game.jdbc.picturepoker;
-
-public class JDBCExecutor {
-    public static void main (String[]args){
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("db", "picturepoker", "postgres", "password");
-    }
-}

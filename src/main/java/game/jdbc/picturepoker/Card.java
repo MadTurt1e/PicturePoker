241290324ef992274726f5366de042dbcdc31555
Jeffrey Wong <jeffrey.wong@cooper.edu>
2024-02-25 20:23:47 -0500

    Create changeSuit method for cards

--- src/main/java/game/jdbc/picturepoker/Card.java
+++ src/main/java/game/jdbc/picturepoker/Card.java
@@ -1,6 +1,8 @@
 // this is where the file is located
 package game.jdbc.picturepoker;
 
+import Java.lang.Math.random;
+
 public class Card {
     // Final to insure that the suit is not changed after a card is created
     private Suit suit;
@@ -9,12 +11,14 @@ public class Card {
 
     // Enum for the suit of the card
     public enum Suit {
-        STAR, MARIO, LUIGI, FIRE_FLOWER, MUSHROOM, CLOUD
+        STAR(5), MARIO(4), LUIGI(3), FIRE_FLOWER(2), MUSHROOM(1), CLOUD(0);
+        public final int strength;
     }
 
     // Constructor
     public Card(Suit suit) {
         this.suit = suit;
+        this.toChange = false;
     }
 
     // Getter for the suit
@@ -22,6 +26,36 @@ public class Card {
         return suit;
     }
 
+    // Suit setter
+
+    public void setSuit(Suit suit){
+        this.suit = suit;
+    }
+    
+    public boolean getToChange(){
+        return toChange;
+    }
+    
+    public void setToChange(boolean toChange){
+        this.toChange = toChange;
+    }
+
+    public void changeSuit(){
+        if(toChange){
+            int newStrength = (int)(Math.random() * 6);
+            // Mitigate potential out of bounds errors with math.random
+            if(newStrength > 5 || newStrength < 0){
+                newStrength = 0;
+            }
+            for(Suit s : values()){
+                if(s.strength = newStrength){
+                    this.suit = s;
+                    break;
+                }
+            }
+            toChange = false;
+        }
+    }
     // String representation of the card
     // Might be useful for UI, if not we can remove it
     @Override

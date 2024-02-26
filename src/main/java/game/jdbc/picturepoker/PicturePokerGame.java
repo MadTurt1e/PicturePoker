329376eadfb12bf9c30acf7a72edf4e0f22ca648
MadTurt1e <81546749+MadTurt1e@users.noreply.github.com>
2024-02-26 00:20:42 -0500

    SpringBoot sample code added

--- src/main/java/game/jdbc/picturepoker/PicturePokerGame.java
+++ src/main/java/game/jdbc/picturepoker/PicturePokerGame.java
@@ -18,6 +18,54 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootApplication
 @RestController
 public class PicturePokerGame {
+    @GetMapping("/helloClass")
+    public String helloClass() {
+        System.out.println("HELLO VAIBHAV");
+        return "Hello Vaibhav";
+    }
+
+    @GetMapping("/getByPlayerName/{playerName}")
+    public Player getByPlayerName(@PathVariable("playerName") String playerName) {
+        System.out.println(playerName);
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("db",
+                "rps2", "postgres", "password");
+        Player player = new Player();
+        player.setPlayerName(playerName);
+        try {
+            Connection connection = dcm.getConnection();
+            PlayerDAO playerDAO = new PlayerDAO(connection);
+
+            player = playerDAO.findById(player.getID());
+            System.out.println(player);
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return player;
+    }
+
+    @PostMapping("/createNewPlayer")
+    public Player createNewPlayer(@RequestBody String json) throws JsonProcessingException {
+        System.out.println(json);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> inputMap = objectMapper.readValue(json, Map.class);
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("db",
+                "picturepoker", "postgres", "password");
+        Player player = new Player();
+        try {
+            Connection connection = dcm.getConnection();
+            PlayerDAO playerDAO = new PlayerDAO(connection);
+            player.setPlayerName(inputMap.get("playerName"));
+            player.setPasscode(inputMap.get("password"));
+            player.setRoundsWon(Integer.parseInt(inputMap.get("win")));
+            player = playerDAO.create(player);
+            System.out.println(player);
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return player;
+    }
     public static void main (String[]args){
         SpringApplication.run(PicturePokerGame.class, args);
     }

2c8abb7335a3f3eab08c558dee2cd473952563bf
Jeffrey Wong <jeffrey.wong@cooper.edu>
2024-02-26 18:28:34 -0500

    Fix double quotes in suits.sql

--- Database SQL/setup.sql
+++ Database SQL/setup.sql
@@ -6,4 +6,4 @@ INSERT INTO player (p_id, p_name, passcode) VALUES (4, 'Richard', 'insecurepassw
 INSERT INTO player (p_id, p_name, passcode) VALUES (5, 'Malek', 'password123');
 
 INSERT INTO game (p1_id, p2_id, p3_id, p4_id, num_rounds, active_players, pot_quantity, difficulty)VALUES(1, 3, 4, 5, 5, 4, 100, 1);
-INSERT INTO game (p1_id, p2_id, p3_id, p4_id, num_rounds, active_players, pot_quantity, difficulty)VALUES(1, 2, 4, 5, 7, 4, 20, 0);
+INSERT INTO game (p1_id, p2_id, p3_id, p4_id, cur_round, num_rounds, active_players, pot_quantity, difficulty)VALUES(1, 2, 4, 5, 7, 7, 4, 20, 0);

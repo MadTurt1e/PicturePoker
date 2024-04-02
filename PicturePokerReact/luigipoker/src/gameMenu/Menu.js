74a47b5e9cc4e7b062b6f9a1acc57bd3be8c53a1
MadTurt1e <81546749+MadTurt1e@users.noreply.github.com>
2024-04-02 01:27:20 -0400

    I have definitely done something here today.

---
+++ PicturePokerReact/luigipoker/src/gameMenu/Menu.js
@@ -0,0 +1,55 @@
+import "./Menu.css";
+
+import logo from '../resources/menuIcons/luigi poker.png'
+import create from '../resources/menuIcons/creategame.png'
+import join from '../resources/menuIcons/joingame.png'
+import statistics from '../resources/menuIcons/statistics.png'
+import exit from '../resources/menuIcons/exit.png'
+import backdrop from '../resources/menuIcons/luigisCasino.jpg'
+
+import React, { useState, useEffect } from 'react';
+
+const images = [create, join, statistics, exit];
+
+
+
+const ImageComponent = () => {
+    const [windowDimensions, setWindowDimensions] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+    // a little thing I got off the internet to adjust the image size based on the size of the window
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    //so basically we set the image size to the smaller of the image dimensions
+    const imageSize = Math.min(windowDimensions.width, windowDimensions.height) * 0.5; // adjust as needed
+
+    return (
+        <div style={{
+            display: 'flex', flexDirection: 'column', alignItems: 'flex-end',
+            backgroundImage: `url(${backdrop})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+            width: '100vw'
+        }}>
+            <img src={logo} alt="" style={{width: imageSize*2, marginBottom: '10px'}}/>
+            {images.map((image, index) => (
+                <button class = "GFG">
+                    <img key={index} src={image} alt="" style={{height: '5vh', marginBottom: '10px'}}/>
+                </button>
+            ))}
+        </div>
+    );
+};
+
+function Menu() {
+    return <ImageComponent/>;
+}
+
+export default Menu

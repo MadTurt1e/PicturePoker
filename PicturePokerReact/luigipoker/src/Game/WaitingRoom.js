dd4b8f6fc49c3f1db762d2d2c5089184ef29e2a0
malek1038 <malek10381038@gmail.com>
2024-04-29 00:31:33 -0400

    Implemented End Of Game Screen UI and Waiting Room UI
    Implemented End Of Game Screen UI and Waiting Room UI

--- PicturePokerReact/luigipoker/src/Game/WaitingRoom.js
+++ PicturePokerReact/luigipoker/src/Game/WaitingRoom.js
@@ -4,60 +4,47 @@ import {useNavigate, useLocation} from "react-router-dom";
 
 import ColorfulText from "../index"
 import backdrop from "../resources/menuIcons/luigisCasino.jpg";
+import "./WaitingRoom.css";
 
 function PlayerList() {
-    const [players, setPlayers] = useState([]);
+    const [players, setPlayers] = useState(null);
     const [pNames, setPNames] = useState([]);
     const location = useLocation();
-    let gid = 0;
+    const gid = location.state.gameId;
     const navigate = useNavigate();
-
-    if (location.state !== null)
-        gid = location.state.gameId;
 
     useEffect(() => {
         //this should be on an interval - it runs once, then it runs every 10 seconds.
         async function doStuff () {
-            //boot bad players out
-            if (gid === 0) {
-                if (sessionStorage.getItem('userID') === null)
-                    navigate('/');
-                else
-                    navigate('/menu')
-            }
-
             const response = await axios.get(`http://localhost:8080/getByGameID/` + gid)
                 .catch(function () {
                     console.log("GetbyGameID didn't work. ");
                 });
-
-            const players = response.data.players;
-            setPlayers(players);
+            setPlayers(response.data.players);
 
             if (players) {
                 const filteredPlayers = players.filter(player => player !== 0);
                 let newPNames = [];
                 for (let i = 0; i < filteredPlayers.length; i++) {
                     let response = await axios.get(`http://localhost:8080/getByPlayerID/${filteredPlayers[i]}`)
-                        .catch(function () {
+                        .catch(function (error) {
                             console.log('getByPlayerID didn\'t work');
                         });
                     newPNames.push(response.data.playerName);
                 }
                 setPNames(newPNames);
-                // Leave the waiting room immediately after we hit 4 players.
-                if (filteredPlayers.length === 4) {
-                    navigate(`/Game`, { state: { gameId: gid } });
-                }
             }
         }
-
         doStuff();
-
         const interval = setInterval(async () => {
             doStuff();
-        }, 3000);
-        //TIME: Set to 3 second delay because we're not really expecting people to stay here for long.
+        }, 10000);
+        //TIME: for debugging purposes. Set to 10 seconds in real life.
+
+        // Leave the waiting room immediately after we hit 4 players.
+        if (pNames.length === 4) {
+            navigate(`/Game`, { state: { gameId: gid } });
+        }
 
         // Cleanup interval on unmount
         return () => clearInterval(interval);
@@ -67,10 +54,10 @@ function PlayerList() {
 
     return (
         <div>
-            <div style={{fontSize:"5vh"}} className={"bordering"}>
+            <div style={{fontSize:"5vh"}} className={"bordering3"}>
                 <ColorfulText text={"Game  ID: " + gid.toString() + " Players required: " + (4 - pNames.length)} />
             </div>
-            <div style={{fontSize:"3vh"}} className={"bordering"}>
+            <div style={{fontSize:"3vh"}} className={"bordering3"}>
                 {pNames.map((value) =>
                     <ColorfulText text={value}/>
                 )}
@@ -86,13 +73,13 @@ function WaitingRoom(){
     const exitGame = async() => {
         //leave game
         let response = await axios.delete(`http://localhost:8080/leaveCurrentGame/${sessionStorage.getItem('userID')}`)
-            .catch(function(){
+            .catch(function(error){
                 console.log("leaveCurrentGame didn\'t work. ");
             });
         if (response.status === 200){
             //if we actually leave, go to the menu
             let response2 = await axios.get(`http://localhost:8080/getPlayerActiveGame/${sessionStorage.getItem('userID')}`)
-                .catch(function(){
+                .catch(function(error){
                     console.log("GetPlayerActiveGame didn\'t work. ");
                 });
             if (response2.data === "" || response2.data.players.includes(parseInt(sessionStorage.getItem("userID"))))
@@ -109,13 +96,11 @@ function WaitingRoom(){
             height: '100vh',
             width: '100vw'
         }}>
-            <div style={{fontSize: "4vh"}} className={"bordering"}>
+            <div style={{fontSize: "4vh"}} className={"header2"}>
                 <ColorfulText text={message}/>
             </div>
-            <PlayerList/>
-            <br/>
-
-            <button className="escapeGame bordering glow" style={{fontSize:"3vh"}} onClick={() => exitGame()}>
+                <PlayerList/>
+            <button className="escapeGame1 bordering3 glow" style={{fontSize:"3vh"}}onClick={() => exitGame()}>
                 <ColorfulText text={"Leave Game?  "}/>
             </button>
 
